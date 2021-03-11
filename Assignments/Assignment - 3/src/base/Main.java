ddc997496ae48ca6ec2a4218b1943c502ae1e137
LukeHart4 <luke.hart1@marist.edu>
2021-03-11 16:20:40 -0500

    Rename

--- Assignments/Assignment - 3/src/base/Main.java
+++ Assignments/Assignment - 3/src/base/Main.java
@@ -1,8 +1,8 @@
-package com.company;
+package base;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
