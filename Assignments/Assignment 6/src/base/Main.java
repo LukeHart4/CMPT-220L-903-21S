946541f02ef742b7c07a008b466c5a382f7d291d
LukeHart4 <luke.hart1@marist.edu>
2021-04-20 15:56:41 -0400

    Assignment 6 second commit

--- Assignments/Assignment 6/src/base/Main.java
+++ Assignments/Assignment 6/src/base/Main.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class Main {
 
     public static void main(String[] args) {
-        Plant cactus = new Plant(true, "succulent", "cactus");
+        Plant cactus = new Plant(true, "succulent", "cactus", false);
         cactus.wateringInstructions();
         cactus.sunlightRequirements();
     }
--- Assignments/Assignment 6/src/base/Plant.java
+++ Assignments/Assignment 6/src/base/Plant.java
@@ -1,28 +1,40 @@
 package base;
 
+// Class
 public class Plant {
 
         boolean isPlant;
         boolean needsWatering;
         String plantFamily;
         String name;
+        boolean needsSunlight;
 
-        public Plant(boolean needsWatering, String plantFamily, String name) {
-            System.out.println("The Plant Constructor Ran.");
+        // Constructor
+        public Plant(boolean needsWatering, String plantFamily, String name, boolean needsSunlight) {
             this.isPlant = true;
             this.needsWatering = needsWatering;
             this.plantFamily = plantFamily;
             this.name = name;
+            this.needsSunlight = needsSunlight;
         }
+
+        // Method
         public void wateringInstructions () {
             if (this.needsWatering == true) {
-                System.out.println("The " + this.name + " needs water to survive.");
+                System.out.println("The " + this.name + " needs more water.");
             }
             else {
-                System.out.println("The " + this.name + " does not need to be watered.");
+                System.out.println("The " + this.name + " has gotten enough water.");
             }
         }
+
+        // Method
         public void sunlightRequirements () {
-            System.out.println("All plants need sunlight!");
+            if (this.needsSunlight == true) {
+                System.out.println("The " + this.name + " needs more sunlight.");
+            }
+            else {
+                System.out.println("The " + this.name + " has gotten enough sunlight.");
+            }
         }
 }
