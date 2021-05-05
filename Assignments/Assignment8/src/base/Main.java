0b818e9f2d783b6ab185769016147651d02b4850
LukeHart4 <luke.hart1@marist.edu>
2021-05-04 22:43:28 -0400

    Update Main.java

--- Assignments/Assignment8/src/base/Main.java
+++ Assignments/Assignment8/src/base/Main.java
@@ -3,6 +3,52 @@ package base;
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        int linearTerm = fibonacciLinear(7, 0, 1);
+        System.out.println("Linear Term: " + linearTerm);
+
+        int headTerm = fibonacciHead(7);
+        System.out.println("Head End Term: " + headTerm);
+
+        int tailTerm = fibonacciTail(7);
+        System.out.println("Tail End Term: " + tailTerm);
     }
+
+    // Linear recursion
+    public static int fibonacciLinear (int term, int x, int y) {
+        if (term == 0) {
+            return x;
+        }
+        else if (term == 1) {
+            return y;
+        }
+        else
+            return fibonacciLinear(term - 1, y, x + y);
+    }
+
+    // Head end recursion
+    public static int fibonacciHead (int term) {
+        if (term > 1) {
+            return fibonacciHead(term - 1) + fibonacciHead(term - 2);
+        }
+        else if (term == 1) {
+            return term;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    // Tail End recursion
+    public static int fibonacciTail (int term) {
+        if (term == 0 ) {
+            return term;
+        }
+        else if (term == 1) {
+            return term;
+        }
+        else
+            return fibonacciTail(term - 1) + fibonacciTail(term - 2);
+    }
+
 }
